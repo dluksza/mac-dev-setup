38939e282c05cd8258f7ffd4e6de72c603cc6564
Dariusz Luksza <dariusz.luksza@gmail.com>
2024-08-04 21:09:55 +0100

    Initial add of `initial_setup.sh`
    First version of script to start initialization process on brand new Mac
    machine.

---
+++ initial_setup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+echo -n 'Installing XCode Command Line Tools...'
+xcode-select --install
+echo ' DONE'
+
+echo -n 'Upbrading Python3 ...'
+sudo pip3 install --upgrade pip
+echo ' DONE'
+
+echo -n 'Installing Ansible ...'
+pip3 install ansible
+echo ' DONE'
+
+echo -n 'Provisioning using `ansible-pull`'
+ansible-pull https://github.com/dluksza/dev-mac-setup
