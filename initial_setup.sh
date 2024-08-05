6d1fb4f6db604003a6cbc1b64e3f59d7df65f6c7
Dariusz Luksza <dariusz.luksza@gmail.com>
2024-08-06 00:51:58 +0100

    Update initial_setup.sh

--- initial_setup.sh
+++ initial_setup.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/env sh
 
-echo -n 'Updating system ...'
-softwareupdate --install --all --restart
+echo 'Updating system ...'
+softwareupdate --install --all --restart &> /dev/null
 echo 'DONE'
 
-echo -n 'Installing XCode Command Line Tools...'
-xcode-select --install
-echo ' DONE'
+if ! (xcode-select -p); then
+    echo 'Installing Command Line Tools (you can track progress in System Settings > General > Software Update)...'
+    xcode-select --install &> /dev/null
+    softwareupdate --install --all
+    echo ' DONE'
+fi
 
-echo -n 'Upbrading Python3 ...'
+echo 'Upgading Python3 ...'
 sudo pip3 install --upgrade pip
 echo ' DONE'
 
-echo -n 'Installing Ansible ...'
+echo 'Installing Ansible ...'
 pip3 install ansible
 echo ' DONE'
 
 echo -n 'Provisioning using `ansible-pull`'
-ansible-pull -U https://github.com/dluksza/dev-mac-setup.git
+~/Library/Python/3.9/bin/ansible-pull -U https://github.com/dluksza/mac-dev-setup.git
